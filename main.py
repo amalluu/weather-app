322e34d1b7094d8422d79b55b1c384edac2762ff
Amalu Kuruvilla <amalukuruvilla1234@gmail.com>
2025-07-28 02:28:56 +0530

    asks for user input

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import os
 load_dotenv() #loads variable from .env
 
 api_key = os.getenv("API_KEY")
-city = "KOCHI"
+
+city = input("Which city would you like weather for?\n")
+print(f"City entered: '{city}'")
+
 
 url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric" 
 
@@ -15,9 +18,15 @@ url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&
 
 response= requests.get(url)
 data = response.json()
+#print("API Response:", data)
 
-temp=data["main"]["temp"]
-description= data["weather"][0]["description"]
-city_name = data["name"]
+if data["cod"] == "404":
+    print("City not found! Please try again.")
+elif data["cod"] == "400":
+    print("Please enter a city name!")
+else:
+    temp=data["main"]["temp"]
+    description= data["weather"][0]["description"]
+    city_name = data["name"]
 
-print (f"The weather in {city_name} is {temp} °C with {description}" )
+    print (f"The weather in {city_name} is {temp} °C with {description}" ) 
