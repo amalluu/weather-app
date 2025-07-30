a7eccdcdafa2b3fe011d01e84ccf9a4cc5788298
Amalu Kuruvilla <amalukuruvilla1234@gmail.com>
2025-07-30 15:14:20 +0530

    Connected form to Flask and retrieved city input

--- web/app.py
+++ web/app.py
@@ -2,7 +2,7 @@
 import requests
 from dotenv import load_dotenv
 import os
-from flask import Flask , render_template #importing flask and render_template is a function for rendering templates 
+from flask import Flask , render_template ,request #importing flask # render_template is a function for rendering templates # request imports the 'request' object from the Flask framework
 
 load_dotenv()
 api_key = os.getenv("API_KEY") #Environment setup should happen before you define routes(web app instance)
@@ -15,6 +15,9 @@ def hello():
 
 @app.route('/results', methods=['POST'])#processes the form and shows weather #methods=['POST'] allow the route to accept POST requests
 def show_results():
-    return "Weather results here"
+    
+    city = request.form.get("cityname")#request.form is a dictionary which contains the submitted form data(only when form method is POST)# .get("cityname")  SIMILAR AS dict.get("key") - MORE SAFE
+    print(city)
+    return f"You searched city for :{city}"
 if __name__ =="__main__": #to run the app
-    app.run()
+    app.run(debug=True)
