4f2ff7f53d3793da598b362f5882e401bab2d349
Amalu Kuruvilla <amalukuruvilla1234@gmail.com>
2025-07-30 12:03:19 +0530

    Add environment setup for API key

--- web/app.py
+++ web/app.py
@@ -1,10 +1,18 @@
-#importing flask
-from flask import Flask
-app = Flask (__name__)# create an instance of app and stores in app variable. __name__ refers to the current Python module
 
+import requests
+from dotenv import load_dotenv
+import os
+from flask import Flask#importing flask
+
+load_dotenv()
+api_key = os.getenv("API_KEY") #Environment setup should happen before you define routes(web app instance)
+
+app = Flask (__name__)# this creates the web app instance. __name__ refers to the current Python module
 @app.route('/')
+
+
 def hello():
     return 'Hello from Flask'
 
-if __name__ =="__main__":
+if __name__ =="__main__": #to run the app
     app.run()
